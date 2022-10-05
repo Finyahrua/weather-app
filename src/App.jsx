6c866b6ee608db5c55981732b0a8b0be63577c11
Finyahrua <finaddy@gmail.com>
2022-10-05 15:59:18 +0300

    done with search and filters component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
-import { Banner, NavBar } from "./components";
+import { Banner, NavBar, SearchAndFilters } from "./components";
 
 function App() {
   return (
     <div className="flex h-screen w-full flex-1 bg-[url('./assets/images/Home.png')] bg-cover">
-      <div className="mx-[80px] w-full mt-[51px]">
+      <div className="mx-[80px] mt-[51px] w-full">
         <NavBar />
-        <Banner/>
+        <Banner />
+        <SearchAndFilters />
       </div>
     </div>
   );
